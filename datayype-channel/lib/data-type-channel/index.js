ef6d80776be86475d0af8e441de986d17983994d
Jonny Quarta <quarta@epsitec.ch>
2022-05-12 15:49:52 +0200

    Complete datatype exercise

--- datayype-channel/lib/data-type-channel/index.js
+++ datayype-channel/lib/data-type-channel/index.js
@@ -1,66 +1,77 @@
 #!/usr/bin/env node
 
-var amqp = require('amqplib/callback_api');
+var amqp = require("amqplib/callback_api");
 
 const exchangeName = "practical-messaging-datatype";
 
-var afterChannelOpened  = function(cb){
-    var me = this;
-    amqp.connect(me.brokerUrl, function(err, conn) {
-        if (err) {
-            console.error("[AMQP]", err.message);
+var afterChannelOpened = function (cb) {
+  var me = this;
+  amqp.connect(me.brokerUrl, function (err, conn) {
+    if (err) {
+      console.error("[AMQP]", err.message);
+      throw err;
+    }
+
+    conn.createConfirmChannel(function (err, channel) {
+      if (err) {
+        console.error("AMDP", err.message);
+        throw err;
+      }
+
+      //we don't usually use this for point-to-point which can be the default exchange
+      channel.assertExchange(
+        exchangeName,
+        "direct",
+        { durable: true },
+        function (err, ok) {
+          if (err) {
+            console.error("AMQP", err.message);
             throw err;
+          }
         }
+      );
 
-        conn.createConfirmChannel(function(err, channel){
-            if (err) {
-                console.error("AMDP", err.message);
-                throw err;
-            }
+      channel.assertQueue(
+        me.queueName,
+        { durable: false, exclusive: false, autoDelete: false },
+        function (err, ok) {
+          if (err) {
+            console.error("AMQP", err.message);
+            throw err;
+          }
+        }
+      );
 
-            //we don't usually use this for point-to-point which can be the default exchange
-            channel.assertExchange(exchangeName, 'direct', {durable:true}, function (err, ok) {
-                if (err){
-                    console.error("AMQP", err.message);
-                    throw err;
-                }
+      channel.bindQueue(
+        me.queueName,
+        exchangeName,
+        me.queueName,
+        {},
+        function (err, ok) {
+          if (err) {
+            console.error("AMQP", err.message);
+            throw err;
+          } else {
+            cb(channel);
+          }
+        }
+      );
 
-            });
-
-            channel.assertQueue(me.queueName, {durable: false, exclusive: false, autoDelete:false}, function(err,ok){
-                if (err){
-                    console.error("AMQP", err.message);
-                    throw err;
-                }
-
-            });
-
-            channel.bindQueue(me.queueName, exchangeName, me.queueName, {}, function(err, ok){
-                if (err){
-                    console.error("AMQP", err.message);
-                    throw err;
-                }
-                else{
-                    cb(channel);
-                }
-            });
-
-
-            setTimeout(function() {
-                channel.close();
-                conn.close();
-            }, 500);
-        });
+      /* setTimeout(function () {
+        channel.close();
+        conn.close();
+      }, 500);*/
     });
+  });
 };
 
 //queueName - the name of the queue we want to create, which ia also the routing key in the default exchange
 //url - the amqp url for the rabbit broker, must begin with amqp or amqps
 //serialize - serialize objects of a given type to the message body (in a dynamic language that can see a little pointless)
 function Producer(queueName, url, serialize) {
-    this.queueName = queueName;
-    this.brokerUrl = url;
-    this.serialize = serialize;
+  this.queueName = queueName;
+  this.brokerUrl = url;
+  this.serialize = serialize;
 }
 
 module.exports.Producer = Producer;
@@ -68,27 +79,32 @@ module.exports.Producer = Producer;
 //cb - the callback to send or receive
 Producer.prototype.afterChannelOpened = afterChannelOpened;
 
-
 //channel - the RMQ channel to make requests on
 //message - the data to serialize
 //cb a callback indicating success or failure
-Producer.prototype.send = function(channel, request, cb){
-    var me = this;
-    channel.publish(exchangeName, this.queueName, Buffer.from(me.serialize(request)), {}, function(err,ok){
-       if (err){
-            console.error("AMQP", err.message);
-            throw err;
-        }
-        cb()
-    });
+Producer.prototype.send = function (channel, request, cb) {
+  var me = this;
+  channel.publish(
+    exchangeName,
+    this.queueName,
+    Buffer.from(me.serialize(request)),
+    {},
+    function (err, ok) {
+      if (err) {
+        console.error("AMQP", err.message);
+        throw err;
+      }
+      cb();
+    }
+  );
 };
 
 //queueName - the name of the queue we want to create, which ia also the routing key in the default exchange
 //url - the amqp url for the rabbit broker, must begin with amqp or amqps
 function Consumer(queueName, url, deserialize) {
-    this.queueName = queueName;
-    this.brokerUrl = url;
-    this.deserialize = deserialize;
+  this.queueName = queueName;
+  this.brokerUrl = url;
+  this.deserialize = deserialize;
 }
 
 module.exports.Consumer = Consumer;
@@ -98,21 +114,20 @@ Consumer.prototype.afterChannelOpened = afterChannelOpened;
 
 //channel - the RMQ channel to make requests on
 //cb a callback indicating success or failure
-Consumer.prototype.receive = function(channel, cb){
-    var me = this;
-    channel.get(this.queueName, {noAck:true}, function(err, msgOrFalse){
-        if(err){
-            console.error("AMQP", err.message);
-        }
-        else if (msgOrFalse === false){
-            cb({});
-        }
-        else {
-            //TODO: Call the deserialize method registered  for this channel
-            cb(request);
-        }
-    });
+Consumer.prototype.receive = function (channel, cb) {
+  var me = this;
+  channel.get(this.queueName, { noAck: true }, function (err, msgOrFalse) {
+    if (err) {
+      console.error("AMQP", err.message);
+    } else if (msgOrFalse === false) {
+      cb({});
+    } else {
+      //TODO: Call the deserialize method registered  for this channel
+      if (msgOrFalse.content) {
+        cb(me.deserialize(msgOrFalse.content.toString()));
+      } else {
+        cb({});
+      }
+    }
+  });
 };
-
-
-
--- datayype-channel/receiver.js
+++ datayype-channel/receiver.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 
-var dataTypeLib =  require("./lib/data-type-channel/");
+var dataTypeLib = require("./lib/data-type-channel/");
 var greetingLib = require("./greetings.js");
 
 var done = false;
 
-const dataTypeChannel = new dataTypeLib.Consumer("practical.messaging.datatype." + new greetingLib.Greetings().constructor.name, "amqp://guest:guest@localhost:5672", function(message){
-//TODO: deserialize the message
-});
+const dataTypeChannel = new dataTypeLib.Consumer(
+  "practical.messaging.datatype." +
+    new greetingLib.Greetings().constructor.name,
+  "amqp://guest:guest@localhost:5672",
+  function (message) {
+    return JSON.parse(message);
+  }
+);
 
 console.log("Preparing to receive message from consumers");
 
-dataTypeChannel.afterChannelOpened(function(channel){
-    dataTypeChannel.receive(channel, function(greeting){
-        console.log('Received Msg: %s', greeting.salutation.toString());
-        done  = true;
-    })
+dataTypeChannel.afterChannelOpened(function (channel) {
+  dataTypeChannel.receive(channel, function (greeting) {
+    console.log("Received Msg: %s", greeting.salutation.toString());
+    done = true;
+  });
 });
 
-
-(function wait () {
-    if (!done) setTimeout(wait, 1000);
+(function wait() {
+  if (!done) setTimeout(wait, 1000);
 })();
